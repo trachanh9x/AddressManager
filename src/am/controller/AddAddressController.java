bd46960d16067d30802b44ec8be124028e8404b7
Mai Hoàng Đức <hoangduc1248@gmail.com>
2016-05-04 20:09:32 +0700

    add comment

--- src/am/controller/AddAddressController.java
+++ src/am/controller/AddAddressController.java
@@ -5,13 +5,6 @@
  */
 package am.controller;
 
-import am.AddressManager;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package am.controller;
-
 import am.AddressManager;
 import am.ConnectToDatabase;
 
