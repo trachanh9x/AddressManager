d51677d2e5a23596cfa9f077648c5965f6f4ea02
Mai Hoàng Đức <hoangduc1248@gmail.com>
2016-05-08 21:01:31 +0700

    Update SearchAddressController.java

--- src/am/controller/SearchAddressController.java
+++ src/am/controller/SearchAddressController.java
@@ -65,14 +65,14 @@ public class SearchAddressController implements Initializable {
                     alert.setContentText("Huyện này không có phường!");
                     alert.showAndWait();
                     
-                    String sql = "select * from ward where name = 'None'";
+                    String sql = "select * from ward where name = 'None'"; // get none from sql.
                     Place ward = new Place();
                     con = new ConnectToDatabase();
                     rs = con.getRS(sql);
                     rs.next();
-                    ward.setId(rs.getString("wardid"));
-                    ward.setName(rs.getString("name"));
-                    ward.setType(rs.getString("type"));
+                    ward.setId(rs.getString("wardid")); // get id of ward.
+                    ward.setName(rs.getString("name")); // get name of ward.
+                    ward.setType(rs.getString("type")); // get type of ward.
                     con.close();
                     
                     addre.setWard(ward);
@@ -89,13 +89,12 @@ public class SearchAddressController implements Initializable {
                     Scene scene = new Scene(root);
                     AddressManager.getStage().setScene(scene);
                 }
-        else searchTable(addressData);   //search address by name.
+        else searchTable(addressData);              //search address by name.
     }
 
     public void searchTable(ObservableList<Place> data) {
         FilteredList<Place> filteredData = new FilteredList<>(data, s -> true); // create filtered list input data.
 
-        //listPlace.setCellFactory(ComboBoxListCell.forListView(data));
         listPlace.setCellFactory(new Callback<ListView<Place>, ListCell<Place>> () {
             @Override
             public ListCell<Place> call(ListView<Place> param) {
@@ -111,7 +110,7 @@ public class SearchAddressController implements Initializable {
                 return cell;
             }
         });
-        listPlace.setItems(filteredData);                                         // add filtered list to list.
+        listPlace.setItems(filteredData);              // add filtered list to list.
         searchText.textProperty().addListener((observable, oldValue, newValue) -> {
             filteredData.setPredicate(s -> {
                 if (newValue == null && newValue.isEmpty()) {
@@ -129,19 +128,19 @@ public class SearchAddressController implements Initializable {
 
             switch (sel) {
                 case 1: // click on province field will input province name and reset number,ward,district data.
-                    addre.setProvince(newValue);// set province = newvalue.
-                    addre.setDistrict(null); // set district =null , district field will empty.
-                    addre.setWard(null);// set ward =null , ward field will empty.
-                    addre.setNumber(null);// set number =null , number field will empty.
+                    addre.setProvince(newValue);    // set province = newvalue.
+                    addre.setDistrict(null);        // set district =null , district field will empty.
+                    addre.setWard(null);            // set ward =null , ward field will empty.
+                    addre.setNumber(null);          // set number =null , number field will empty.
                     break;
                 case 2: // click on district field will input district name and reset number  and ward data.
-                    addre.setDistrict(newValue);// set district = newvalue, districtfield will display newvalue
-                    addre.setWard(null);// set ward =null , ward field will empty.
-                    addre.setNumber(null);// set number =null , number field will empty.
+                    addre.setDistrict(newValue);    // set district = newvalue, districtfield will display newvalue
+                    addre.setWard(null);            // set ward =null , ward field will empty.
+                    addre.setNumber(null);          // set number =null , number field will empty.
                     break;
                 case 3: // click on ward field will input ward name and reset number.
-                    addre.setWard(newValue);// set ward= newvalue, ward will display newvalue
-                    addre.setNumber(null);// set number =null , number field will empty.
+                    addre.setWard(newValue);        // set ward= newvalue, ward will display newvalue
+                    addre.setNumber(null);          // set number =null , number field will empty.
                     break;
             }
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/am/view/AddAddress.fxml")); // load scene AddAddress.
@@ -152,7 +151,7 @@ public class SearchAddressController implements Initializable {
                 Logger.getLogger(SearchAddressController.class.getName()).log(Level.SEVERE, null, ex);
             }
             AddAddressController controller = loader.getController();
-            controller.initDataAdd(addre); // send address data to AddAddressController.java.
+            controller.initDataAdd(addre);                 // send address data to AddAddressController.java.
             Scene scene = new Scene(root);
             AddressManager.getStage().setScene(scene);
         });
@@ -162,19 +161,19 @@ public class SearchAddressController implements Initializable {
     private void addAddressData() throws SQLException {
         Place tmp;
         count = 0;
-        con = new ConnectToDatabase(); // connect to Database.
-        addressData.clear();// clear addressData.
+        con = new ConnectToDatabase();                     // connect to Database.
+        addressData.clear();                               // clear addressData.
         String sql;
-        switch (sel) {  // sel is number of flag. with 1 as province, 2 as district, 3 as ward.
+        switch (sel) {                                     // sel is number of flag. with 1 as province, 2 as district, 3 as ward.
             case 1:
                 sql = "select * from province order by name";// sql question
-                rs = con.getRS(sql);// get rs have province name.
+                rs = con.getRS(sql);                        // get rs have province name.
                 while (rs.next()) {
                     tmp = new Place();
-                    tmp.setId(rs.getString("provinceid"));
-                    tmp.setName(rs.getString("name"));
-                    tmp.setType(rs.getString("type"));
-                    addressData.add(tmp);// add to data.
+                    tmp.setId(rs.getString("provinceid")); // get provinceid from data
+                    tmp.setName(rs.getString("name"));     // get name from data
+                    tmp.setType(rs.getString("type"));     // get type from data
+                    addressData.add(tmp);                  // add element place to data.
                     //count++;
                 }
                 sql = null;
@@ -186,14 +185,14 @@ public class SearchAddressController implements Initializable {
                 sql = sql.concat("'");
                 sql = sql.concat(" order by district.name");// sql question
 
-                rs = con.getRS(sql); // send sql get rs have district name into province.
+                rs = con.getRS(sql);                        // send sql get rs have district name into province.
 
                 while (rs.next()) {
                     tmp = new Place();
-                    tmp.setId(rs.getString("districtid"));
-                    tmp.setName(rs.getString("name"));
-                    tmp.setType(rs.getString("type"));
-                    addressData.add(tmp); // add to data.
+                    tmp.setId(rs.getString("districtid"));  // get district id from data
+                    tmp.setName(rs.getString("name"));      // get name of district from data.
+                    tmp.setType(rs.getString("type"));      // get type from data.
+                    addressData.add(tmp);                   // add to element place data.
                     //count++;
                 }
                 sql = null;
@@ -205,28 +204,28 @@ public class SearchAddressController implements Initializable {
                 sql = sql.concat("'");
                 sql = sql.concat(" order by ward.name");// sql question
 
-                rs = con.getRS(sql);// send sql get rs have ward name into district .
+                rs = con.getRS(sql);                    // send sql get rs have ward name into district .
 
                 while (rs.next()) {
                     tmp = new Place();
-                    tmp.setId(rs.getString("wardid"));
-                    tmp.setName(rs.getString("name"));
-                    tmp.setType(rs.getString("type"));
-                    addressData.add(tmp); // add to data.
+                    tmp.setId(rs.getString("wardid"));  // get wardid from data
+                    tmp.setName(rs.getString("name"));  // get name of ward form data
+                    tmp.setType(rs.getString("type"));  // get type from data.
+                    addressData.add(tmp);               // add to data.
                 }
                 
-                sql = null;// reset sql question.
+                sql = null;                             // reset sql question.
                 break;
             default:
                 break;
         }
 
-        con.close();// close connect to database.
+        con.close();                                    // close connect to database.
     }
 
     @FXML
     private void handleButtonX(ActionEvent event) {
-        searchText.setText(""); // press button x then reset text field
+        searchText.setText("");                         // press button x then reset text field
     }
 
     @Override
